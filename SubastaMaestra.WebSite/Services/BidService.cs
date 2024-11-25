ba83798d2cb4b76abb14dee12f812379f286d467
darioBD <dario.d.monte.d.o@gmail.com>
2024-11-25 13:07:39 -0300

    Revert "ofertar"
    This reverts commit a54d7fca76cb20796f37acc59b6b079496a5945a.

--- SubastaMaestra.WebSite/Services/BidService.cs
+++ SubastaMaestra.WebSite/Services/BidService.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using SubastaMaestra.Models.DTOs.Bid;
-using SubastaMaestra.Models.Utils;
 namespace SubastaMaestra.WebSite.Services
 {
 
@@ -17,39 +16,10 @@ namespace SubastaMaestra.WebSite.Services
             _httpClient = httpClient;
         }
 
-        public async Task<(bool Success, string Message)> CreateBid(BidCreateDTO bid)
+        public async Task<bool> CreateBid(BidCreateDTO bid)
         {
-            try
-            {
-                var response = await _httpClient.PostAsJsonAsync("/api/Bid/create", bid);
-
-                // Variables para el mensaje y éxito
-                string Message = string.Empty;
-                bool success = false;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    // Si la operación fue exitosa
-                    var result = await response.Content.ReadFromJsonAsync<OperationResult<BidCreateDTO>>();
-                    // Procesamos el resultado de la operación exitosa
-                    success = true;
-                    Message = "Oferta realizada exitosamente";  // O el mensaje adecuado que desees mostrar
-                }
-                else
-                {
-                    // Si la operación falló, obtenemos el mensaje de error
-                    Message = await response.Content.ReadAsStringAsync();
-                }
-
-                // Devolvemos el resultado de la operación
-                return (success, Message);
-            }
-            catch (Exception ex)
-            {
-                // Manejo de excepciones en caso de errores en el cliente.
-                return (false, $"Error al realizar la oferta: {ex.Message}");
-            }
+            var response = await _httpClient.PostAsJsonAsync("/api/Bid/create", bid);
+            return response.IsSuccessStatusCode;
         }
-
     }
 }
